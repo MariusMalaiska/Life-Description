329f7380801be83b16e263d71edae8cf2fff0be4
Marius Malaiska <mariusmalaiska@gmail.com>
2020-08-02 13:13:54 +0300

    Updates

---
+++ precache-manifest.aed3aed2bc61ba64117d8d782ed9afd8.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "28f14ff7d0bbefae95d2c5eb43b1a826",
+    "url": "/Life-Description/index.html"
+  },
+  {
+    "revision": "10234f991a49ee5a1bd4",
+    "url": "/Life-Description/static/css/main.011a486d.chunk.css"
+  },
+  {
+    "revision": "468c96ce27af5f592936",
+    "url": "/Life-Description/static/js/2.e552ba56.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Life-Description/static/js/2.e552ba56.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "10234f991a49ee5a1bd4",
+    "url": "/Life-Description/static/js/main.fbfc6deb.chunk.js"
+  },
+  {
+    "revision": "fe36ac91a3cdebe028e1",
+    "url": "/Life-Description/static/js/runtime-main.0e6ec1f7.js"
+  },
+  {
+    "revision": "0fc9d143732cd11d6fbeb4e2fcbc07f0",
+    "url": "/Life-Description/static/media/CSS.0fc9d143.png"
+  },
+  {
+    "revision": "f2282f94c041c37ef4db028e428e41c1",
+    "url": "/Life-Description/static/media/HTML.f2282f94.png"
+  },
+  {
+    "revision": "874a400e33a1a4636d3b40ee3257b8ed",
+    "url": "/Life-Description/static/media/SkillsBackground.874a400e.jpg"
+  },
+  {
+    "revision": "40ba872606d5b33575f43effebdebc64",
+    "url": "/Life-Description/static/media/Vue.40ba8726.png"
+  },
+  {
+    "revision": "c274cd283964ddef320d3fd08afb7be9",
+    "url": "/Life-Description/static/media/ca_logo.c274cd28.png"
+  },
+  {
+    "revision": "ce2f78a83193b00888335604ba11d95b",
+    "url": "/Life-Description/static/media/myImg.ce2f78a8.jpeg"
+  },
+  {
+    "revision": "f8dab57d048fabd69ea16c67e1615b86",
+    "url": "/Life-Description/static/media/node.f8dab57d.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Life-Description/static/media/react.5d5d9eef.svg"
+  },
+  {
+    "revision": "380e22e306f5f0e7ceb6077715651b76",
+    "url": "/Life-Description/static/media/sass.380e22e3.png"
+  }
+]);
